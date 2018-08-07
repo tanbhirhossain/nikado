646592145da20584aefafc24597d64c9f5c191b7
Tanbhir Hossain <r25n.cstanbhir@gmail.com>
2018-08-07 09:28:41 +0600

    Multi Auth

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Admin;
+use Illuminate\Http\Request;
+use Auth;
+use Session;
+use App\role_admin;
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+        $this->middleware('admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin');
+    }
+
+    public function add_user(){
+        return view('backend.user.add_user');
+    }
+
+    public function save_user(Request $request){
+        $this->validate($request, [
+            'name' => 'required|min:3|max:30',
+            'email' => 'required|email|unique:admins,email',
+            'user_role' => 'required',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $admin = new Admin();
+        $admin->name = $request->name;
+        $admin->email = $request->email;
+        $admin->password = bcrypt($request->password);
+        $admin->user_role = $request->user_role;
+        $admin->save();
+
+
+        $admin_id = $admin->id;
+        Session::put('id',$admin_id);
+
+        $ar = new role_admin();
+        $ar->admin_id = $admin_id;
+        $ar->role_id = $request->user_role;
+        $ar->save();
+
+        return back()->with('message_success','User Added Successfully.....');
+    }
+
+    public function user_list(){
+        $all_user = Admin::all();
+        return view('backend.user.user_list',compact('all_user'));
+    }
+
+
+
+
+
+}
---
+++ database/migrations/2017_03_06_023521_createAdminTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->tinyInteger('user_role')->nullable()->comment('1 admin, 2 moderator, 3 editor');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Admins');
+    }
+}
